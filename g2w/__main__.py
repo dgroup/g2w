5d1d42798c27d8ce3f3af5fb6cdd9fd856857eb8
Daniil Bondarenko <sumasedsiefakty@gmail.com>
2022-02-18 09:44:26 +0200

    #33: Adding prometheus metrics

--- g2w/__main__.py
+++ g2w/__main__.py
@@ -10,11 +10,13 @@ from fastapi import Request, FastAPI, Response, status
 from fastapi.routing import APIRouter
 
 from g2w import Push, Ws, LoggableRoute, Alert, Log, __version__
+from prometheus_fastapi_instrumentator import Instrumentator
 
 ws = Ws()
 app = FastAPI(version=__version__, title="g2w")
 router = APIRouter(route_class=LoggableRoute)
 log = logging.getLogger(f"g2w.{__name__}")
+Instrumentator().instrument(app).expose(app)
 
 
 @router.post(
--- tests/test_app.py
+++ tests/test_app.py
@@ -22,10 +22,7 @@ class ClientTest(AbstractTest):
     def test_e2e_push(self):
         # ws = 'http://worksection.api'
         # requests_mock.get('http://test.com', text='data')
-        response = TestClient(app).post(
-            "/gitlab/push/223728",
-            json=self.body("sample2_commits.json")
-        )
+        response = TestClient(app).post("/gitlab/push/223728", json=self.body("sample2_commits.json"))
         self.assertEqual(response.status_code, 200)
         self.assertGreater(int(response.json()["comments"][0]["id"]), 0)
 
@@ -33,12 +30,6 @@ class ClientTest(AbstractTest):
     @pytest.mark.skipif(os.getenv("WS_URL_POST_TASK") is None, reason="Environment variable 'WS_URL_POST_TASK' is absent")
     @pytest.mark.skipif(os.getenv("WS_INT_TESTS_DISABLED") is not None, reason="Integration tests are disabled")
     def test_e2e_alert(self):
-        response = TestClient(app).post(
-            "/grafana/alert/223728",
-            json=self.body("sample1_alert.json")
-        )
+        response = TestClient(app).post("/grafana/alert/223728", json=self.body("sample1_alert.json"))
         self.assertEqual(response.status_code, 200)
         self.assertGreater(response.json()["created"]["id"], 0)
-
-
-
